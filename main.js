566bf08d31673c2eaa4f989eec9900d7857886a3
forestdweller77 <forestdwellertv@gmail.com>
2023-06-29 21:46:00 -0500

    trying again

--- main.js
+++ main.js
@@ -12,12 +12,33 @@ const rl = readline.createInterface({
 });
 
 // the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+  
+  const rockPaperScissors = (hand1, hand2) => {
 
-}
+    
+
+
+    if (hand1 == hand2) {
+        return "It's a tie!"; 
+      }  
+      else if (hand1 == "rock" && hand2 == "scissors"){
+        return "Hand one wins!"
+      } else if (hand1 == "paper" && hand1 == "rock"){
+        return "Hand one wins!"}
+        else if (hand1 == "scissors" && hand2 == "paper"){
+          return "Hand one wins!"
+        } else if (hand1 == "rock" && hand2 == "paper"){
+          return "Hand two wins!"}
+          else if (hand1 == "paper" && hand2 == "scissors"){
+            return "Hand two wins!"}
+            else if (hand1 == "scissors" && hand2 == "rock"){
+              return "Hand two wins!"
+            }
+          }
+  
+
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
